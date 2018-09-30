c3caab2cf2d0b1e906755ace27683bbdd879d7f4
Andy Chen <amazingandyyy@gmail.com>
2018-09-30 11:07:22 -0700

    clean up

--- src/actions/types.js
+++
@@ -1,8 +0,0 @@
-export const RESET = 'reset';
-
-export const AUTH_USER = 'auth_user';
-export const AUTH_ADMIN = 'auth_admin';
-export const UNAUTH_USER = 'unauth_user';
-export const AUTH_ERROR = 'auth_error';
-
-export const TRY_CONNECT = 'try_connect';
