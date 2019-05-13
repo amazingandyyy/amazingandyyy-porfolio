44af8c2d92cff541c7b516df92c3520d6a5d44e8
amazingandyyy <andycandomore@gmail.com>
2019-05-13 12:01:55 -0700

    add works

--- src/components/home/timeline/index.js
+++ src/components/home/timeline/index.js
@@ -1,6 +1,10 @@
+
 import React from 'react';
 import './style.scss'
 export default () => (<div className='timeline-component'>
+    <div className='title-component'>
+        Timeline
+    </div>
     <p className='side'>Built <b>OnePressKit</b> side project <span className='date'>2019/04</span></p>
     <p>Joined <b>Minted</b> Software Architecture Team <span className='date'>2019/03</span></p>
     <p className='move'>Daly City ✈ San Francisco in 2019</p>
