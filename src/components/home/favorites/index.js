fa36ab8fbe79c1f7af98f11bb629f41b0c06ecf7
amazingandyyy <andycandomore@gmail.com>
2019-07-19 18:36:41 -0700

    update

--- src/components/home/favorites/index.js
+++ src/components/home/favorites/index.js
@@ -4,7 +4,7 @@ import FavModule from './module';
 
 export default () => (<div className='works-component'>
     <div className='title-component'>
-        Favorites
+        Things that inspire me
     </div>
     <FavModule />
 </div>)
