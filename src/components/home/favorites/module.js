502e54cb2e9fb33d9eb7f2a419e533ed7fd15493
amazingandyyy <andycandomore@gmail.com>
2019-07-31 18:05:19 -0700

    need style

--- src/components/home/favorites/module.js
+++ src/components/home/favorites/module.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.scss';
 import ReactMarkdown from 'react-markdown';
 
@@ -8,47 +8,56 @@ function open(link){
     if(link && link!=="") window.open(link, '_blank');
 }
 
-const items = [];
-// &times;
 export default () => {
     const [item, setItem] = useState({});
     const [expand, setExpand] = useState(false);
     const [expandWhole, setExpandWhole] = useState(false);
+    const [order, setOrder] = useState([]);
+    
     const reset = () => {setItem({}); setExpand(false); setExpandWhole(false)};
-    const openDrawer = (p) => {setExpand(true);setItem(p)};
+    const openDrawer = (p, i) => {
+        setExpand(true);
+        setItem(p)
+        const keys = Object.keys(data);
+        keys.splice(i+1, 0, "DISPLAY");
+        setOrder(keys);
+    };
+    useEffect(() => {
+        setOrder(Object.keys(data));
+    }, []);
     const expandDrawer = () => {setExpandWhole(true)};
-    Object.keys(data).forEach(k=>{
-        const d = data[k];
-        const i = {d, k};
-        items.push(i);
-    })
     return (<div className='fav-display'>
-        <div className={`pin-${expand} display-drawer ${expand} full-${expandWhole}`}>
-            <div className='name'>{item.name}</div>
-            <div className='check-it' onClick={()=>open(item.link)}>Check it out ></div>
-            <span className={`expand ${expandWhole}`} onClick={expandDrawer}>
-                <i className="fas fa-chevron-up"></i>
-            </span>
-            <span className='close' onClick={reset}>
-                <i className="fas fa-chevron-down"></i>
-            </span>
-            {item.intro ? <div className='intro'><ReactMarkdown source={item.intro} /></div>:<div className='intro text-center'>{`More introduction coming up!`}</div>}
-        </div>
-        {items.map(({d, k})=>{
-            return (<div key={k} className='fav-module'>
-                <div className='fav-module-title'>{`${k}`}</div>
-                <div className={`display-container ${k.split(' ').map(i=>i.toLowerCase()).join('-')}`}>
-                    {d.list.map(p=>(<div className={`display-item pin-${item.name===p.name || !expand}`} key={p.name} onClick={()=>openDrawer(p)}>
-                        <div className='card' style={{width: `${d.meta.width}`}}>
-                            <div className='frame' style={{height: `${d.meta.height}`, width: `${d.meta.width}`}}>
-                                <img loading='lazy' alt={p.name} src={p.img} />
+        {order.length>0&&order.map((k, i)=>{
+            if(k=='DISPLAY'){
+                const index = order.indexOf('DISPLAY');
+                order.splice(index, 1);
+                return (<div key={k} className='fav-module'>
+                    <div className={`pin-${expand} display-detail ${expand} full-${expandWhole}`}>
+                        <div className='name'>{item.name}</div>
+                        <div className='check-it' onClick={()=>open(item.link)}>Check it out ></div>
+                        <span className='close' onClick={reset}>
+                            <i className="fas fa-chevron-down"></i>
+                        </span>
+                        {item.intro ? <div className='intro'><ReactMarkdown source={item.intro} /></div>:<div className='intro text-center'>{`More introduction coming up!`}</div>}
+                    </div>
+                </div>)
+            }else if(data[k]){
+                const d = data[k];
+                return (<div key={k} className='fav-module'>
+                    <div className='fav-module-title'>{`${k}`}</div>
+                    <div className={`display-container ${k.split(' ').map(i=>i.toLowerCase()).join('-')}`}>
+                        {d.list.map(p=>(<div className={`display-item pin-${item.name===p.name || !expand}`} key={p.name} onClick={()=>openDrawer(p, i)}>
+                            <div className='card' style={{width: `${d.meta.width}`}}>
+                                <div className='frame' style={{height: `${d.meta.height}`, width: `${d.meta.width}`}}>
+                                    <img loading='lazy' alt={p.name} src={p.img} />
+                                </div>
+                                <p className='name'>{p.name}</p>
+                                {p.notes&&<p className='notes'>{`${p.notes}`}</p>}
                             </div>
-                            <p className='name'>{p.name}</p>
-                            {p.notes&&<p className='notes'>{`${p.notes}`}</p>}
-                        </div>
-                    </div>))}
-                </div>
-            </div>)
+                        </div>))}
+                    </div>
+                </div>)
+            }
         })}
     </div>)
 }
